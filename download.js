fc9de399e03a506090bdd86601a0ed61645c8f0f
karthik raman naig <karthik.naig@gmail.com>
2024-10-08 21:11:57 +0530

    Delete download.js

--- download.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const zipPath = path.join(__dirname, 'project.zip');
-const zipContent = fs.readFileSync(zipPath);
-const base64Zip = zipContent.toString('base64');
-
-console.log(JSON.stringify({ filename: 'project.zip', content: base64Zip }));
